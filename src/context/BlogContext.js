0794bfdeee3d26b023c165634f290a5827992409
Mieszko Borodziuk <mieszko.borodziuk@gmail.com>
2021-01-10 20:53:54 +0100

    Update

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,8 +4,18 @@ import creaateDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                if (blogPost.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return blogPost;
+                }
+            });
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}` }];
+            return [...state, { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content }];
         default:
             return state;
     }
@@ -13,10 +23,30 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-    return () => {
-        dispatch({ type: 'add_blogpost' });
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: { title, content } });
+        if (callback) {
+            callback();
+        }
     }
 };
 
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    }
+};
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({
+            type: 'edit_blogpost',
+            payload: { id, title, content }
+        });
+        if (callback) {
+            callback();
+        }
+    };
+};
 
-export const { Context, Provider } = creaateDataContext(blogReducer, { addBlogPost }, []);
+
+export const { Context, Provider } = creaateDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'Test POST', content: 'Test Content', id: 1 }]);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,26 +1,42 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <View style={styles.row}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Feather style={styles.icon} name="trash" />
-          </View>
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          )
         }}
       />
     </View>
   );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
